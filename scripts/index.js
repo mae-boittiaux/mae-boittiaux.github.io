be2aaa2144c00a6d3568dc7a3113ef4b83e739d7
mae-boittiaux <maeboittiaux@gmail.com>
2025-07-28 15:15:57 +0100

    feat(phone): render main components of application as nook-phone with apps for projects / content

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+const title = document.getElementById('title');
+
+const mapIcon = document.getElementById('map-icon');
+const chatIcon = document.getElementById('chat-icon');
+const passportIcon = document.getElementById('passport-icon');
+
+mapIcon.onmouseenter = () => {
+    title.innerHTML = 'Offline Map';
+};
+
+chatIcon.onmouseenter = () => {
+    title.innerHTML = 'Contact';
+};
+
+passportIcon.onmouseenter = () => {
+    title.innerHTML = 'About';
+};
+
+mapIcon.onmouseleave = () => {
+    title.innerHTML = 'Mae Boittiaux';
+};
+
+chatIcon.onmouseleave = () => {
+    title.innerHTML = 'Mae Boittiaux';
+};
+
+passportIcon.onmouseleave = () => {
+    title.innerHTML = 'Mae Boittiaux';
+};
+
+function updateTime() {
+    const time = document.getElementById('time');
+    time.innerHTML = new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" });
+    sleep(1000).then(() => { updateTime(); });
+}
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+updateTime();
